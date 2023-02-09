c3a2f7d3140173fa3bcfae84286b1b95c95d117e
Anna Beatriz Dizon <annabeatriz.dizon@towerswatson.com>
2023-02-10 01:20:46 +0800

    Code cleanup for index component

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Category } from 'src/app/models/category';
 import { Item } from 'src/app/models/item';
-import { CategoryService } from 'src/app/services/to-do/category.service';
 import { ItemService } from 'src/app/services/to-do/item.service';
 
 @Component({
@@ -9,37 +7,6 @@ import { ItemService } from 'src/app/services/to-do/item.service';
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
+
 export class IndexComponent {
-  items: Item[] = []
-  categoryId: number
-
-  constructor(private itemService: ItemService) {}
-
-  ngOnInit() : void {
-    console.log("NgOnInit executing on index component..");
-    this.itemService.getAllItems().subscribe((items) => {
-      this.items = items;
-    })
-  }
-
-  formEventHandler = (payload : Item) => {
-    console.log("Executing formEventHandler..");
-
-    if (this.items.length > 0) {
-      payload.id = (this.items[this.items.length-1].id as number) + 1;
-    }
-    else {
-      payload.id = 1;
-    }
-
-    console.log(payload)
-    this.items.push(payload)
-  }
-
-  setCategoryIdHandler = (payload: any) => {
-    console.log("Executing setCategoryIdHandler..");
-
-    this.categoryId = payload.categoryId
-    console.log(`Category ID: ${this.categoryId}`)
-  }
 }
